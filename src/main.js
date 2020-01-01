edccdf21f911a08aeb3bd86066ea8bc436f9a5e2
Fardeen Khan <khanfardeen9983@gmail.com>
2020-01-02 02:20:52 +0530

    Refactoring

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-Vue.use(BootstrapVue)
-
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -10,13 +8,17 @@ import store from './store'
 import './registerServiceWorker'
 import VueResource from 'vue-resource'
 
+Vue.use(BootstrapVue);
 
-Vue.use(VueResource)
 
-Vue.config.productionTip = false
+Vue.use(VueResource);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+    router,
+    store,
+    render: function (h) {
+        return h(App)
+    }
+}).$mount('#app');
